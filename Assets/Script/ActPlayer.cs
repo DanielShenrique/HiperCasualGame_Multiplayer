a041f5242756201f75d5f2ce6a6e458b5604165b
Aluno <aluno@cejoseleitelopes.com.br>
2018-10-18 16:44:58 -0300

    2
    2 - Inicio do Network

--- Assets/Script/ActPlayer.cs
+++ Assets/Script/ActPlayer.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class ActPlayer : MonoBehaviour {
+public class ActPlayer : NetworkBehaviour {
 
     void Update()
     {
+		if(isLocalPlayer){
         Vector3 temp = Input.mousePosition;
         temp.z = 10f;
         this.transform.position = Camera.main.ScreenToWorldPoint(temp);
+		}
     }
 }
